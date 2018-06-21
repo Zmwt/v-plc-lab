9c7dc036b00e1a76ccc4cbbd3b683e264dbd8c5b
Ziemowit Niezgoda <ziem.asd@gmail.com>
2018-06-21 15:37:51 +0200

    s

--- PLCLAB/Controllers/TaskController.cs
+++ PLCLAB/Controllers/TaskController.cs
@@ -11,10 +11,13 @@ namespace PLCLAB.Controllers
     public class TaskController : Controller
     {
         // GET: Task
+        /*
         public ActionResult Index()
         {
             return View();
         }
+        */
+
 
         [HttpPost]
         public ActionResult Set(FormCollection col)
@@ -23,31 +26,18 @@ namespace PLCLAB.Controllers
             using (TaskService ts = new TaskService())
             {
                 TaskDefinition td = ts.NewTask();
-                //td.RegistrationInfo.Description = "PLC Task"; // opis taska
+                td.RegistrationInfo.Description = "PLC Task"; // opis taska
 
-                //td.Triggers.Add(new  { DaysInterval = 2 });
-
-                //TimeTrigger tTrig = new TimeTrigger(DateTime.Now + TimeSpan.FromMinutes(1));
                 TimeTrigger tTrig = new TimeTrigger(DateTime.Now);
                 tTrig.StartBoundary = DateTime.Now + TimeSpan.FromMinutes(Convert.ToDouble(col["delay"]));
                 tTrig.Repetition.Interval = TimeSpan.FromMinutes(Convert.ToDouble(col["interval"])); // 1 min
                 tTrig.EndBoundary = DateTime.Now + TimeSpan.FromMinutes(Convert.ToDouble(col["duration"])) + TimeSpan.FromMinutes(Convert.ToDouble(col["delay"]));
-                /*tTrig.Repetition.Interval = TimeSpan.FromSeconds(3);
-                tTrig.EndBoundary = DateTime.Now + TimeSpan.FromSeconds(10);*/
                 td.Triggers.Add(tTrig);
                 if (Convert.ToDouble(col["delay"]) == 0)
                 {
                     td.Triggers.Add(new RegistrationTrigger { Delay = TimeSpan.FromMinutes(0) });
                 }
-                //td.Actions.Add(new ExecAction("notepad.exe", "/w zyd zydek zydek"));
-                //string teststr = "";
-                //teststr += "zyd ";
-                //teststr += "zydek ";
-                //teststr += "zydek";
-
-                //td.Actions.Add(new ExecAction(@"C:\paramtest\paramtest.exe", teststr));
-
-
+                
                 string str = col["id"] + " "
                     + col["ip"] + " "
                     + col["port"] + " "
@@ -56,7 +46,6 @@ namespace PLCLAB.Controllers
                     + col["quantity"]// + " "
                                      //+ taskid
                 ;
-
 
                 td.Actions.Add(new ExecAction(Server.MapPath("~/paramtest/paramtest.exe"), str));
 
